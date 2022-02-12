bfa05773c8479978ec99512e81d362c57a7d95f1
Anyazagibalova01 <zagibalova2001@bk.ru>
2022-02-12 11:34:10 +0300

    Второе задание

---
+++ typeof.js
@@ -0,0 +1,9 @@
+let user = {age:15};
+console.log (typeof user); 
+let str = "Аня904";
+console.log (typeof str);
+let num = 14;
+console.log(typeof num);
+let age = 789;
+console.log(typeof age);
+
