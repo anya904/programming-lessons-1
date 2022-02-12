35780b6832f677bd447516ad4f0d0f6b9cdd53fc
Anyazagibalova01 <zagibalova2001@bk.ru>
2022-02-12 12:04:39 +0300

    Задание2

--- typeof.js
+++ typeof.js
@@ -4,6 +4,13 @@ let str = "Аня904";
 console.log (typeof str);
 let num = 14;
 console.log(typeof num);
-let age = 789;
-console.log(typeof age);
+let cif = 789;
+console.log(typeof cif);
+let True, False = true;
+console.log(typeof true);
+let sim = Symbol();
+console.log (typeof sim);
+
+
+
 
