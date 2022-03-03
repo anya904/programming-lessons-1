9b022ba2b019c4510b00ce69dcfefd70eee2bd1b
Anyazagibalova01 <zagibalova2001@bk.ru>
2022-03-03 16:01:59 +0300

    Добавление системы логирования

--- veb8.js
+++ veb8.js
@@ -1,24 +1,28 @@
+const fs = require("fs");
 const http = require("http");
 
 
 const requestHandler = (request, response) => {
      response.setHeader("Content-Type", "text/html; charset=utf-8;");
-
      if(request.url === "/home" || request.url === "/"){
           response.write("<h2>Home</h2>");
+          fs.appendFileSync("text.txt", "Home")
           }
-          else if(request.url == "/about"){
+     else if(request.url == "/about"){
           response.write("<h2>About</h2>");
+          fs.appendFileSync("text.txt", "About")
           }
-          else if(request.url == "/contact"){
+     else if(request.url == "/contact"){
           response.write("<h2>Contacts</h2>");
+          fs.appendFileSync("text.txt", "Contacts")
           }
-          else{
+     else{
           response.write("<h2>Not found</h2>");
+          fs.appendFileSync("text.txt", "Not found")
           }
           response.end();
-          };
-          http.createServer(requestHandler).listen(3000);   
+     };
+         
+ http.createServer(requestHandler).listen(3000);   
 
 
-http.createServer(requestHandler).listen(3000);
